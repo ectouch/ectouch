80ce2c749ec88bcd6b993a4da4596d20cb443453
vagrant <wanganlin@ecmoban.com>
2014-10-19 14:30:28 +0800

    更新版本到beta2

--- uploads/data/version.php
+++ uploads/data/version.php
@@ -1,5 +1,5 @@
 <?php
 define('APPNAME', 'ECTOUCH');
-define('VERSION', '1.0Beta1');
-define('RELEASE', '20141016');
+define('VERSION', '1.0Beta2');
+define('RELEASE', '20141019');
 define('ECTOUCH_AUTH_KEY', '');
