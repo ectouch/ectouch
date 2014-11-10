35180afc0aa9a25a9f9a8c15e66bb7b956adfe39
vagrant <wanganlin@ecmoban.com>
2014-11-10 11:17:07 +0800

    增强model基类

--- uploads/include/EcModel.class.php
+++ uploads/include/EcModel.class.php
@@ -24,6 +24,9 @@ class EcModel {
     public $pre = ''; //表前缀，主要用于在其他地方获取表前缀
     public $config = array(); //配置
     protected $options = array(); // 查询表达式参数	
+    public $queryTime = ''; // sql运行时间
+    public $queryCount = 0; // sql运行次数
+    public $queryLog   = array();
 
     /**
      * 构造函数
@@ -91,6 +94,17 @@ class EcModel {
             return false;
         $sql = str_replace('{pre}', $this->pre, $sql); //表前缀替换
         $this->sql = $sql;
+        if ($this->queryCount++ <= 99)
+        {
+            $this->queryLog[] = $sql;
+        }
+        if ($this->queryTime == '') {
+            if (PHP_VERSION >= '5.0.0') {
+                $this->queryTime = microtime(true);
+            } else {
+                $this->queryTime = microtime();
+            }
+        }
         //判断当前的sql是否是查询语句
         if ($is_query || stripos(trim($sql), 'select') === 0) {
             $data = $this->_readCache();
@@ -353,6 +367,182 @@ class EcModel {
         $this->options['limit'] = '';
         $this->options['field'] = '*';
         return $condition;
+    }
+    
+    /*********************************************/
+    
+    // 返回最后一次插入数据库的ID号
+    function insert_id(){
+        return $this->db->insert_id();
+    }
+    
+    // 返回错误信息
+    function error() {
+        return mysql_error();
+    }
+    
+    // 返回错误代号
+    function errno() {
+        return mysql_errno();
+    }
+    
+    //输出错误信息
+    public function ErrorMsg($message = '') {
+        $error = $this->error();
+        $errorno = $this->errno();
+        if (DEBUG) {
+            $str = " {$message}<br>
+            <b>SQL</b>: {$this->sql}<br>
+            <b>错误详情</b>: {$error}<br>
+            <b>错误代码</b>:{$errorno}<br>";
+            } else {
+            $str = "<b>出错</b>: $message<br>";
+            }
+            throw new Exception($str);
+    }
+    
+    function autoReplace($table, $field_values, $update_values, $where = '', $querymode = '') {
+    $query = $this->db->query('DESC ' . $table);
+    while ($row = $this->db->fetchArray($query)) {
+    $field_descs[] = $row;
+    }
+    if (!is_array($field_descs)) {
+    $field_descs = array();
+    }
+    $primary_keys = array();
+    foreach ($field_descs AS $value) {
+    $field_names[] = $value['Field'];
+    if ($value['Key'] == 'PRI') {
+    $primary_keys[] = $value['Field'];
+    }
+    }
+    
+        $fields = $values = array();
+        foreach ($field_names AS $value) {
+            if (array_key_exists($value, $field_values) == true) {
+            $fields[] = $value;
+            $values[] = "'" . $field_values[$value] . "'";
+    }
+    }
+    
+            $sets = array();
+            foreach ($update_values AS $key => $value) {
+                if (array_key_exists($key, $field_values) == true) {
+                if (is_int($value) || is_float($value)) {
+                $sets[] = $key . ' = ' . $key . ' + ' . $value;
+    } else {
+    $sets[] = $key . " = '" . $value . "'";
+    }
+    }
+    }
+    
+    $sql = '';
+    if (empty($primary_keys)) {
+        if (!empty($fields)) {
+        $sql = 'INSERT INTO ' . $table . ' (' . implode(', ', $fields) . ') VALUES (' . implode(', ', $values) . ')';
+        }
+        } else {
+        if (mysql_get_server_info() >= '4.1') {
+        if (!empty($fields)) {
+        $sql = 'INSERT INTO ' . $table . ' (' . implode(', ', $fields) . ') VALUES (' . implode(', ', $values) . ')';
+            if (!empty($sets)) {
+            $sql .= 'ON DUPLICATE KEY UPDATE ' . implode(', ', $sets);
+        }
+        }
+        } else {
+        if (empty($where)) {
+            $where = array();
+            foreach ($primary_keys AS $value) {
+            if (is_numeric($value)) {
+                $where[] = $value . ' = ' . $field_values[$value];
+            } else {
+            $where[] = $value . " = '" . $field_values[$value] . "'";
+            }
+            }
+                    $where = implode(' AND ', $where);
+            }
+    
+            if ($where && (!empty($sets) || !empty($fields))) {
+                if (intval($this->getOne("SELECT COUNT(*) FROM $table WHERE $where")) > 0) {
+                if (!empty($sets)) {
+                    $sql = 'UPDATE ' . $table . ' SET ' . implode(', ', $sets) . ' WHERE ' . $where;
+                    }
+                    } else {
+                    if (!empty($fields)) {
+                        $sql = 'REPLACE INTO ' . $table . ' (' . implode(', ', $fields) . ') VALUES (' . implode(', ', $values) . ')';
+                }
+                }
+                }
+                }
+            }
+            if ($sql) {
+            return $this->query($sql, $querymode);
+            } else {
+            return false;
+            }
+            }
+    
+            // 返回一条数据
+            function getRow($sql, $limited = false) {
+                if ($limited == true) {
+                    $sql = trim($sql . ' LIMIT 1');
+            }
+            $res = $this->query($sql);
+            if ($res !== false) {
+            return $res['0'];
+            } else {
+            return false;
+            }
+            }
+    
+            //  函数返回结果集中一个字段的值
+            function result($query, $row) {
+            return @mysql_result($query, $row);
+            }
+    
+            // 返回结果集的条数
+            function num_rows($query) {
+            return mysql_num_rows($query);
+            }
+    
+            // 返回结果集中的字段数
+            function num_fields($query) {
+            return mysql_num_fields($query);
+    }
+    
+            function free_result($query) {
+            return mysql_free_result($query);
+            }
+    
+            // 返回根据从结果集取得的行生成的关联数组
+            function fetchRow($query) {
+            return mysql_fetch_assoc($query);
+            }
+    
+            function fetch_fields($query) {
+                return mysql_fetch_field($query);
+    }
+    
+    // 返回mysql版本号
+    function version() {
+        return mysql_get_server_info();
+    }
+    
+    // 如果存在连接，则返回 true。如果失败，则返回 false。
+    function ping() {
+        if (PHP_VERSION >= '4.3') {
+            return mysql_ping();
+        } else {
+            return false;
+        }
+    }
+    
+    function escape_string($unescaped_string) {
+        if (PHP_VERSION >= '4.3') {
+            return mysql_real_escape_string($unescaped_string);
+        } else {
+            return mysql_escape_string($unescaped_string);
+        }
     }
 
 }
