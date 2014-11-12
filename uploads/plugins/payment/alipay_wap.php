297183d60bdcf2e15a7471644900dbe28f473c4d
vagrant <wanganlin@ecmoban.com>
2014-11-12 15:12:28 +0800

    增加支付消息推送

--- uploads/plugins/payment/alipay_wap.php
+++ uploads/plugins/payment/alipay_wap.php
@@ -233,6 +233,13 @@ class alipay_wap
             if ($trade_status == 'TRADE_FINISHED' || $trade_status == 'TRADE_SUCCESS') {
                 /* 改变订单状态 */
                 model('Payment')->order_paid($log_id, 2);
+                if(method_exists('WechatController', 'do_oauth')){
+                    /* 如果需要，微信通知 wanglu */
+                    $order_id = model('Base')->model->table('order_info')->field('order_id')->where('order_sn = "'.$out_trade_no[0].'"')->getOne();
+                    $order_url = __HOST__ . url('user/order_detail', array('order_id'=>$order_id));
+                    $order_url = urlencode(base64_encode($order_url));
+                    send_wechat_message('pay_remind', '', $out_trade_no[0].' 订单已支付', $order_url, $out_trade_no[0]);
+                }
                 echo "success";
             } else {
                 echo "fail";
