cbb1fa97212912e009653491f3564cad074336ac
vagrant <wanganlin@ecmoban.com>
2014-12-19 11:56:48 +0800

    更新版本信息

--- uploads/data/version.php
+++ uploads/data/version.php
@@ -1,5 +1,5 @@
 <?php
 define('APPNAME', 'ECTouch');
-define('VERSION', '1.0Beta3');
-define('RELEASE', '20141028');
+define('VERSION', '1.0');
+define('RELEASE', '20141218');
 define('ECTOUCH_AUTH_KEY', '');
