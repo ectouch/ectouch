a464a83fa8cb47d55edcea4ff6feab8654187d91
vagrant <wanganlin@ecmoban.com>
2014-12-19 11:29:27 +0800

    更新短信发送内容

--- uploads/include/apps/default/controller/SmsController.class.php
+++ uploads/include/apps/default/controller/SmsController.class.php
@@ -66,8 +66,8 @@ class SmsController extends CommonController {
             }
         }
 
-        $mobile_code = $this->random(6, 1);
-        $message = "您的验证码是：" . $this->mobile_code . "，请不要把验证码泄露给其他人，如非本人操作，可不用理会！";
+        $this->mobile_code = $this->random(6, 1);
+        $message = "您的验证码是：" . $this->mobile_code . "。请不要把验证码泄露给其他人。";
 
         $sms = new EcsSms();
         $sms_error = '';
