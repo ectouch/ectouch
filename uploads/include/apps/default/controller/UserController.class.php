1c96af94d8ff07aa246903db1f8a767650308c97
carson <wanganlin@ecmoban.com>
2015-03-24 17:59:56 +0800

    优化支付方式的描述显示

--- uploads/include/apps/default/controller/UserController.class.php
+++ uploads/include/apps/default/controller/UserController.class.php
@@ -656,12 +656,7 @@ class UserController extends CommonController {
         $order['order_status'] = L('os.' . $order['order_status']);
         $order['pay_status'] = L('ps.' . $order['pay_status']);
         $order['shipping_status'] = L('ss.' . $order['shipping_status']);
-        // 如果是银行汇款或货到付款 则显示支付描述
-        $payment = model('Order')->payment_info($order ['pay_id']);
-        if ($payment['pay_code'] == 'bank' || $payment['pay_code'] == 'balance'){
-            $this->assign('pay_desc',$payment['pay_desc']);
-        }
-		
+
         $this->assign('title', L('order_detail'));
         $this->assign('order', $order);
         $this->assign('goods_list', $goods_list);
