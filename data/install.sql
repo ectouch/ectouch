dcf15f50d109802f8a531d2179d4613a6b66c8b3
wangshuai <wangshuai@ecmoban.com>
2015-04-27 17:58:21 +0800

    添加导航栏图片

--- data/install.sql
+++ data/install.sql
@@ -251,14 +251,14 @@ CREATE TABLE IF NOT EXISTS `ecs_touch_nav` (
 --
 
 INSERT INTO `ecs_touch_nav` (`id`, `ctype`, `cid`, `name`, `ifshow`, `vieworder`, `opennew`, `url`, `pic`, `type`) VALUES
-(1, '', 0, '全部分类', 1, 0, 0, 'index.php?c=category&amp;a=top_all', 'data/attached/nav/c78d6a6b9b1ef0f58760c1c26fcd1ed3.png', 'middle'),
-(2, '', 0, '我的订单', 1, 0, 0, 'index.php?m=default&amp;c=user&amp;a=order_list', 'data/attached/nav/fa2f3f5df8dfa7ca5740515d47d2381d.png', 'middle'),
-(3, '', 0, '最新团购', 1, 0, 0, 'index.php?m=default&amp;c=groupbuy', 'data/attached/nav/0c71ca825682cad7222266a7e7cd052a.png', 'middle'),
-(4, '', 0, '促销活动', 1, 0, 0, 'index.php?m=default&amp;c=activity', 'data/attached/nav/ca0a1b9798403546b2b3b9ccdc7a3fcc.png', 'middle'),
-(5, '', 0, '热门搜索', 1, 0, 0, 'javascript:openSearch();', 'data/attached/nav/cea26362c1ba667b48ff26d5a7f06fe1.png', 'middle'),
-(6, '', 0, '品牌街', 1, 0, 0, 'index.php?m=default&amp;c=brand', 'data/attached/nav/d4eaf0ee8bf517fb0d7368481a170df0.png', 'middle'),
-(7, '', 0, '个人中心', 1, 0, 0, 'index.php?m=default&amp;c=user', 'data/attached/nav/b0e139079e5c5d052f1f05d0f400dfa1.png', 'middle'),
-(8, '', 0, '购物车', 1, 0, 0, 'index.php?m=default&amp;c=flow&amp;a=cart', 'data/attached/nav/3355eb17b4a10ab97ac5bf4d5ceef3ef.png', 'middle');
+(1, '', 0, '全部分类', 1, 0, 0, 'index.php?c=category&amp;a=top_all', 'themes/default/images/nav/nav_0.png', 'middle'),
+(2, '', 0, '我的订单', 1, 0, 0, 'index.php?m=default&amp;c=user&amp;a=order_list', 'themes/default/images/nav/nav_1.png', 'middle'),
+(3, '', 0, '最新团购', 1, 0, 0, 'index.php?m=default&amp;c=groupbuy', 'themes/default/images/nav/nav_2.png', 'middle'),
+(4, '', 0, '促销活动', 1, 0, 0, 'index.php?m=default&amp;c=activity', 'themes/default/images/nav/nav_3.png', 'middle'),
+(5, '', 0, '热门搜索', 1, 0, 0, 'javascript:openSearch();', 'themes/default/images/nav/nav_4.png', 'middle'),
+(6, '', 0, '品牌街', 1, 0, 0, 'index.php?m=default&amp;c=brand', 'themes/default/images/nav/nav_5.png', 'middle'),
+(7, '', 0, '个人中心', 1, 0, 0, 'index.php?m=default&amp;c=user', 'themes/default/images/nav/nav_6.png', 'middle'),
+(8, '', 0, '购物车', 1, 0, 0, 'index.php?m=default&amp;c=flow&amp;a=cart', 'themes/default/images/nav/nav_7.png', 'middle');
 
 --
 -- 表的结构 `ecs_touch_payment`
