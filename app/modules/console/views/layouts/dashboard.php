e3b8f4f589872d05a2884e051aaf3d61a41035b5
carson <docxcn@gmail.com>
2017-02-08 18:36:42 +0800

    增加后台layout

---
+++ app/modules/console/views/layouts/dashboard.php
@@ -0,0 +1,31 @@
+<?php
+
+use app\assets\DashboardAsset;
+use yii\helpers\Html;
+
+DashboardAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <div class="container">
+        <?= $content ?>
+    </div>
+</div>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
