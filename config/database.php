44900552c9ca03cb62f6229e6efe8b9b3c25cf3c
carson <docxcn@gmail.com>
2017-02-08 15:07:13 +0800

    更新默认数据库名称

--- config/database.php
+++ config/database.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ectouch_thunder',
+    'dsn' => 'mysql:host=localhost;dbname=ecshop_db',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
-    'tablePrefix' => null
+    'tablePrefix' => 'ecs_'
 ];
