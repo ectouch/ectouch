5ae6c5b9d7d2aff7cc404f1d5698a1f2a701500c
carson <docxcn@gmail.com>
2017-02-08 16:12:25 +0800

    增加后台验证控制器

---
+++ app/modules/console/controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\modules\console\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Auth controller for the `console` module
+ */
+class AuthController extends Controller
+{
+    /**
+     * Renders the login view for the module
+     * @return string
+     */
+    public function actionLogin()
+    {
+        return $this->render('login');
+    }
+
+    /**
+     * Renders the forgot view for the module
+     * @return string
+     */
+    public function actionForgot()
+    {
+        return $this->render('forgot');
+    }
+
+    /**
+     * Renders the reset view for the module
+     * @return string
+     */
+    public function actionReset()
+    {
+        return $this->render('reset');
+    }
+}
