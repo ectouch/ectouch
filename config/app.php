371abc344d16814dbec7537fdf8c431bdedbaa26
carson <docxcn@gmail.com>
2017-02-09 14:08:47 +0800

    更新应用配置文件

--- config/app.php
+++ config/app.php
@@ -1,5 +1,45 @@
 <?php
 
 return [
-    'name' => 'ECTouch'
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application. This value is used when the
+    | framework needs to place the application's name in a notification or
+    | any other location as required by the application or its packages.
+    */
+
+    'name' => 'ECTouch',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application URL
+    |--------------------------------------------------------------------------
+    |
+    | This URL is used by the console to properly generate URLs when using
+    | the Artisan command line tool. You should set this to the root of
+    | your application so that it is used when running Artisan tasks.
+    |
+    */
+
+    'url' => 'http://m.ectouch.cn',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Static Resource URL
+    |--------------------------------------------------------------------------
+    */
+
+    'static' => 'http://m.ectouch.cn/public',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Static Resource Path
+    |--------------------------------------------------------------------------
+    */
+
+    'storage' => dirname(__DIR__) . '/storage/app/public',
 ];
