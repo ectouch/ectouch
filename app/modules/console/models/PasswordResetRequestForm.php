462944b719babeb5a0ad6390e63852ee16b68adb
carson <docxcn@gmail.com>
2017-02-09 11:41:41 +0800

    增加后台表单模型

---
+++ app/modules/console/models/PasswordResetRequestForm.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\modules\console\models;
+
+use Yii;
+use yii\base\Model;
+use app\models\AdminUser;
+
+/**
+ * Password reset request form
+ */
+class PasswordResetRequestForm extends Model
+{
+    public $email;
+
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            ['email', 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'exist',
+                'targetClass' => '\app\models\AdminUser',
+                'filter' => ['status' => AdminUser::STATUS_ACTIVE],
+                'message' => 'There is no user with this email address.'
+            ],
+        ];
+    }
+
+    /**
+     * Sends an email with a link, for resetting the password.
+     *
+     * @return bool whether the email was send
+     */
+    public function sendEmail()
+    {
+        /* @var $user AdminUser */
+        $user = AdminUser::findOne([
+            'status' => AdminUser::STATUS_ACTIVE,
+            'email' => $this->email,
+        ]);
+
+        if (!$user) {
+            return false;
+        }
+        
+        if (!AdminUser::isPasswordResetTokenValid($user->password_reset_token)) {
+            $user->generatePasswordResetToken();
+            if (!$user->save()) {
+                return false;
+            }
+        }
+
+        return Yii::$app
+            ->mailer
+            ->compose(
+                ['html' => 'passwordResetToken-html', 'text' => 'passwordResetToken-text'],
+                ['user' => $user]
+            )
+            ->setFrom([Yii::$app->params['supportEmail'] => Yii::$app->name . ' robot'])
+            ->setTo($this->email)
+            ->setSubject('Password reset for ' . Yii::$app->name)
+            ->send();
+    }
+}
