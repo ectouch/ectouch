e3155dd2758ed75b0a1f930a4f6115d328dd0b92
carson <docxcn@gmail.com>
2017-02-10 08:53:47 +0800

    增加apidoc基本样式

--- app/modules/apidoc/views/default/index.php
+++ app/modules/apidoc/views/default/index.php
@@ -3,7 +3,14 @@
 use yii\helpers\Html;
 
 ?>
-
+<style>
+.apidoc-default-index {
+    background: white;
+    margin: 10px 0;
+    padding: 5px 18px;
+    border-radius: 10px;
+}
+</style>
 <div class="apidoc-default-index">
     <?= $content ?>
 </div>
