88f0d8f980415444fb82c3f20363ac973caf81b9
xiaorui <xiaorui@ecmoban.com>
2018-04-24 17:03:58 +0800

    提交修复优惠活动后台限制

--- admin/favourable.php
+++ admin/favourable.php
@@ -236,6 +236,81 @@ elseif ($_REQUEST['act'] == 'insert' || $_REQUEST['act'] == 'update') {
     /* 是否添加 */
     $is_add = $_REQUEST['act'] == 'insert';
 
+    /** 验证一个商品只能参加一个活动start**/
+    $now = gmtime();
+    $act_id = intval($_POST['id']);
+    $act_range = intval($_POST['act_range']);
+    $act_range_ext = isset($_POST['act_range_ext']) && !empty($_POST['act_range_ext']) ? implode(",", $_POST['act_range_ext']) : '';
+
+    //全部商品
+    $act_range_all_goods = get_all_goods($act_id, $_POST['act_type']);
+    if ($act_range_all_goods == 1)
+    {
+        sys_msg($_LANG['lab_act_range_desc'][4], 1);
+    }
+
+    // 按分类优惠活动包含的所有商品
+    $act_range_ext_cat = get_act_range_ext(FAR_CATEGORY, $act_id);
+    $goods_list_cat = get_range_goods(FAR_CATEGORY, $act_range_ext_cat, 'cat_id');
+  
+    // 按品牌优惠活动包含的所有商品
+    $act_range_ext_brand = get_act_range_ext(FAR_BRAND, $act_id);
+    $goods_list_brand = get_range_goods(FAR_BRAND, $act_range_ext_brand, 'brand_id');
+    
+    // 按商品优惠活动包含的所有商品
+    $act_range_ext_goods = get_act_range_ext(FAR_GOODS, $act_id);
+    $goods_list_goods = get_range_goods(FAR_GOODS, $act_range_ext_goods, 'goods_id');
+
+    switch ($act_range)
+    {
+        case 0:// 全部商品
+            $where = '';
+            if($act_id){
+                $where .= " AND act_id <> '$act_id'";
+            }
+            $sql = "SELECT COUNT(*) FROM ".$GLOBALS['ecs']->table('favourable_activity'). " WHERE  end_time >= '$now' AND act_range = 0  $where";
+            $num = $GLOBALS['db']->getOne($sql);
+            if ($num)
+            {
+                sys_msg($_LANG['lab_act_range_desc'][3], 1);
+            }
+            break;     
+        case 1: // 按分类
+            $goods_list_cat_new = get_range_goods(FAR_CATEGORY, $_POST['act_range_ext'], 'cat_id');
+            $arr = array_intersect($goods_list_cat, $goods_list_cat_new);
+            $arr1 = array_intersect($goods_list_brand, $goods_list_cat_new);
+            $arr2 = array_intersect($goods_list_goods, $goods_list_cat_new);
+            if ($arr || $arr1 || $arr2)
+            {
+                sys_msg($_LANG['lab_act_range_desc'][1], 1);
+            }
+            break;
+        case 2: // 按品牌
+            $goods_list_brand_new = get_range_goods(FAR_BRAND, $_POST['act_range_ext'], 'brand_id');
+            $arr = array_intersect($goods_list_cat, $goods_list_brand_new);
+            $arr1 = array_intersect($goods_list_brand, $goods_list_brand_new);
+            $arr2 = array_intersect($goods_list_brand_new, $goods_list_goods);
+            if ($arr || $arr1 || $arr2)
+            {
+                sys_msg($_LANG['lab_act_range_desc'][2], 1);
+            }
+            break;
+        case 3: // 按商品
+            $goods_list_goods_new = get_range_goods(FAR_GOODS, $_POST['act_range_ext'], 'goods_id');
+            $arr = array_intersect($goods_list_cat, $goods_list_goods_new);
+            $arr1 = array_intersect($goods_list_brand, $goods_list_goods_new);
+            $arr2 = array_intersect($goods_list_goods, $goods_list_goods_new);    
+            if ($arr || $arr1 || $arr2)
+            {
+                sys_msg($_LANG['lab_act_range_desc'][0], 1);
+            }
+            break;
+        default:
+            break;
+    }
+
+    /** 验证一个商品只能参加一个活动end**/
+
     /* 检查名称是否重复 */
     $act_name = sub_str($_POST['act_name'], 255, false);
     if (!$exc->is_only('act_name', $act_name, intval($_POST['id']))) {
@@ -463,4 +538,106 @@ function favourable_list()
     }
 
     return array('item' => $list, 'filter' => $filter, 'page_count' => $filter['page_count'], 'record_count' => $filter['record_count']);
+}
+
+// 同一类型优惠范围（分类或品牌） -qin
+function get_act_range_ext($act_range, $act_id)
+{
+    if ($act_range > 0)
+    {
+        $a_range = " AND act_range = '$act_range' ";
+    }
+    $now = gmtime();
+   
+    $sql = "SELECT act_range_ext " .
+            "FROM " . $GLOBALS['ecs']->table('favourable_activity') .
+            " WHERE  end_time >= '$now' " . $a_range ." AND act_id <> '$act_id'  ";
+
+    $res = $GLOBALS['db']->getAll($sql);
+
+    $arr=array();
+    foreach ($res as $key => $row)
+    {
+        $arr = array_merge($arr, explode(',', $row['act_range_ext']));
+    }
+    
+    return array_unique($arr);
+}
+
+// 获取分类或品牌下得所有商品
+function get_range_goods($act_range, $act_range_ext_list, $create_in)
+{
+    if (empty($act_range_ext_list))
+    {
+        return array();
+    }
+    
+    switch ($act_range)
+    {
+        case FAR_CATEGORY:
+
+            $id_list = array();
+            foreach ($act_range_ext_list as $id)
+            {
+
+                /**
+                * 当前分类下的所有子分类
+                * 返回一维数组
+                */
+
+                $cat_keys = get_array_keys_cat(intval($id));
+               
+                $id_list = array_merge($id_list, $cat_keys);
+            }
+            break;
+        case FAR_BRAND:
+            $id_list = $act_range_ext_list;
+            break;
+        case FAR_GOODS:
+            $id_list = $act_range_ext_list;
+            break; 
+
+        default:
+            break;
+    }
+   
+    $sql = "SELECT goods_id FROM ".$GLOBALS['ecs']->table('goods')." WHERE  ".  db_create_in($id_list, $create_in);
+    $res = $GLOBALS['db']->query($sql);
+    $arr_goods_id = array();
+    while ($row = $GLOBALS['db']->fetchRow($res))
+    {
+        $arr_goods_id[] = $row['goods_id'];                                                                                                      
+    }
+    return $arr_goods_id;
+}
+
+/**
+ *获取分类id的一维数组
+ */
+function get_array_keys_cat($cat_id = 0, $type = 0, $table = 'category'){
+    $res = cat_list($cat_id, 0, false);
+    $arr = array();
+    foreach ($res as $key => $val) {      
+            $arr[] = $val['cat_id'];
+    }
+    return $arr;   
+}
+
+/**
+*获取全部商品的优惠活动与现在添加/编辑的优惠活动是否冲突
+**/
+function get_all_goods($act_id, $act_type){
+    $now = gmtime();
+    $sql = "SELECT act_type, act_id  FROM ".$GLOBALS['ecs']->table('favourable_activity')." WHERE act_range = 0 and end_time >= '$now' and  act_id <> '$act_id'";       
+    $res = $GLOBALS['db']->getRow($sql);
+
+    if($res['act_type']){
+        $result = $res['act_type'] == $act_type ? 1 : 0;
+        if($result == 1){
+            return 1;
+        }else{
+            return false;
+        }
+    }
+    return false;
 }
