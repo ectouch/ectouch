bebfe8de98f8ddb9041eada69c16e1eb4a0e9165
carson <wanganlin@ecmoban.com>
2025-12-15 13:58:11 +0800

    refactor: 更新客户端IP地址的获取方法

--- include/base/helpers/function.php
+++ include/base/helpers/function.php
@@ -138,34 +138,39 @@ function html_out($str)
  * 获取客户端IP地址
  * @param integer $type 返回类型 0 返回IP地址 1 返回IPV4地址数字
  * @param boolean $adv 是否进行高级模式获取（有可能被伪装）
- * @return mixed
+ * @return string|int
  */
-function get_client_ip($type = 0, $adv = false)
+function get_client_ip(int $type = 0, bool $adv = false): string|int
 {
     $type = $type ? 1 : 0;
     static $ip = null;
     if ($ip !== null) {
         return $ip[$type];
     }
+    
+    $ipAddress = '0.0.0.0';
+    
     if ($adv) {
-        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+        $ipAddress = $_SERVER['HTTP_X_FORWARDED_FOR'] 
+            ?? $_SERVER['HTTP_CLIENT_IP'] 
+            ?? $_SERVER['REMOTE_ADDR'] 
+            ?? '0.0.0.0';
+            
+        if (str_contains($ipAddress, ',')) {
+            $arr = explode(',', $ipAddress);
             $pos = array_search('unknown', $arr);
-            if (false !== $pos) {
+            if ($pos !== false) {
                 unset($arr[$pos]);
             }
-            $ip = trim($arr[0]);
-        } elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
-            $ip = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (isset($_SERVER['REMOTE_ADDR'])) {
-            $ip = $_SERVER['REMOTE_ADDR'];
+            $ipAddress = trim($arr[0]);
         }
-    } elseif (isset($_SERVER['REMOTE_ADDR'])) {
-        $ip = $_SERVER['REMOTE_ADDR'];
+    } else {
+        $ipAddress = $_SERVER['REMOTE_ADDR'] ?? '0.0.0.0';
     }
+    
     // IP地址合法验证
-    $long = sprintf("%u", ip2long($ip));
-    $ip = $long ? array($ip, $long) : array('0.0.0.0', 0);
+    $long = sprintf("%u", ip2long($ipAddress));
+    $ip = $long ? [$ipAddress, (int)$long] : ['0.0.0.0', 0];
     return $ip[$type];
 }
 
